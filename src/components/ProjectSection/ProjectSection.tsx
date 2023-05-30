f12e091e09841af4373333849c555c7f1be6be91
Aubis Sanchez <aubisst@gmail.com>
2023-05-30 17:04:43 -0400

    styles: project info when hover

--- src/components/ProjectSection/ProjectSection.tsx
+++ src/components/ProjectSection/ProjectSection.tsx
@@ -35,13 +35,13 @@ const ProjectSection = () => {
             key={project.title}
             className={` ${
               showMobileVersion ? 'w-[250px] h-[400px]' : 'w-[500px] h-[350px] '
-            }  bg-lightgray group/item duration-300  border-opacity-[0.05] shadow-lg rounded-lg relative`}
+            }  bg-lightgray group/item duration-300   shadow-lg rounded-lg relative `}
           >
             <img
               src={`${
                 showMobileVersion ? project.mobileImg : project.desktopImg
               }`}
-              className={` w-full object-cover  absolute rounded-lg z-0 ${
+              className={` w-full object-cover   absolute rounded-lg z-0 ${
                 showMobileVersion ? '' : 'h-full'
               }`}
               alt={project.title}
@@ -50,13 +50,20 @@ const ProjectSection = () => {
               initial={{ opacity: 0 }}
               whileHover={{ opacity: 1 }}
               transition={{ duration: 0.4, ease: easeIn, type: 'spring' }}
-              className='invisible flex-col group-hover/item:visible group/info absolute flex items-center justify-center text-gray-900 rounded-lg bg-black bg-opacity-[85%] h-full w-full z-[200]'
+              className='invisible flex-col group-hover/item:visible group/info absolute flex items-center justify-between p-12 text-gray-900 rounded-lg bg-black bg-opacity-[85%] h-full w-full z-[200] '
             >
-              <p className='text-xl font-semibold text-center text-white '>
+              <p className='text-3xl font-bold text-center capitalize text-yellow '>
                 {project.title}
               </p>
-              <div className='absolute flex flex-row items-center w-full bottom-6 justify-evenly'>
-                <button className='flex flex-row items-center px-3 py-2 mt-1 font-semibold border-white rounded-md bg-yellow '>
+              <div className='flex flex-wrap items-center justify-center w-4/5 h-auto pt-4 text-sm text-white'>
+                {project.techs.map((tech) => (
+                  <span className='px-2 py-1 mt-1 ml-1 border border-yellow rounded-xl bg-black/50'>
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className='flex flex-row items-center w-full text-sm justify-evenly'>
+                <button className='flex flex-row items-center px-3 py-1 mt-1 font-semibold duration-300 ease-in bg-blue-400 border-white rounded-md hover:bg-blue-500 '>
                   Live demo
                   <img
                     src={LinkOutIcon}
@@ -65,7 +72,7 @@ const ProjectSection = () => {
                   />
                 </button>
                 <a
-                  className='flex flex-row items-center px-3 py-2 mt-1 font-semibold border-white rounded-md bg-yellow '
+                  className='flex flex-row items-center px-3 py-1 mt-1 font-semibold duration-300 bg-green-400 border-white rounded-md hover:bg-green-500'
                   target='_blank'
                   href=''
                 >
@@ -76,7 +83,7 @@ const ProjectSection = () => {
                     className='w-6 h-6 pl-2 mt-1'
                   />
                 </a>
-                <button className='flex flex-row items-center px-3 py-2 mt-1 font-semibold border-white rounded-md bg-yellow '>
+                <button className='flex flex-row items-center px-3 py-1 mt-1 font-semibold duration-300 border-white rounded-md bg-yellow hover:bg-strong_yellow'>
                   Watch preview
                   <img
                     src={PlayIcon}
