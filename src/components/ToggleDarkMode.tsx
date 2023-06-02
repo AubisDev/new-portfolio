5cde0950526430329ec49f79ac6afe9507187d94
Aubis Sanchez <aubisst@gmail.com>
2023-06-02 02:36:35 -0400

    feat: dark mode icons aded

--- src/components/ToggleDarkMode.tsx
+++ src/components/ToggleDarkMode.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import lightModeIcon from '@/assets/svg/lightmode.svg'
+import darkModeIcon from '@/assets/svg/darkmode.svg'
 
 const ToggleDarkMode = () => {
   const [mode, setMode] = useState('light')
@@ -18,9 +20,25 @@ const ToggleDarkMode = () => {
   }
 
   return (
-    <button onClick={handleClick} className='text-dark_orange dark:text-white'>
-      {mode === 'light' ? 'Toggle Dark Mode' : 'Toggle Light Mode'}
-    </button>
+    <div className='relative flex flex-row'>
+      <label className='relative inline-flex items-center cursor-pointer'>
+        <input
+          type='checkbox'
+          value=''
+          className='sr-only peer'
+          onClick={() => handleClick()}
+        />
+        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        <span className='ml-3 text-sm font-medium text-gray-900 dark:text-gray-300'>
+          {' '}
+          {mode === 'light' ? (
+            <img src={darkModeIcon} alt='light mode icon' />
+          ) : (
+            <img src={lightModeIcon} alt='light mode icon' />
+          )}
+        </span>
+      </label>
+    </div>
   )
 }
 
